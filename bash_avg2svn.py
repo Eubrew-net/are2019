8562dfbd24b3b0f4cef5e7ee75a229f04a67c5fc
jlopezs <jlopezs@c1bf16b6-0dbf-4448-92a5-1278db4b58d5>
2019-05-17 14:44:28 +0000

    Added bash_avg2svn.py, a very simple script to update AVG files

---
+++ bash_avg2svn.py
@@ -0,0 +1,34 @@
+import os
+
+myBrewer="228"
+myCampaign="~/CODE/campaigns/are2019"
+myAVGs=["SLOAVG", "DTOAVG", "RSOAVG", "APOAVG", "HGOAVG", "H2OAVG", "OPAVG"]
+
+# download, unzip, and svn add all files
+for myFile in myAVGs:
+
+    # get
+    myCmd="curl -u ibero:nesia 'http://www.eubrewnet.org/eubrewnet/data/get/Files?brewerid="+\
+            myBrewer+"&type="+myFile+"&date=2019-05-17' > tempEBNdata.zip"
+    os.system(myCmd)
+
+    # unzip
+    myCmd="unzip tempEBNdata.zip -d "+myCampaign+"/bdata"+myBrewer
+    os.system(myCmd)
+
+    # svn add
+    myCmd="svn add "+myCampaign+"/bdata"+myBrewer+"/*"
+    os.system(myCmd)
+
+# now do the svn commit
+myCmd="svn commit -m 'Added AVG files for Brewer "+myBrewer+\
+      "' --username brewersync --password redbrewer "+myCampaign
+os.system(myCmd)
+
+# and update
+myCmd="svn update "+myCampaign
+os.system(myCmd)
+
+# finally, delete the temp file
+myCmd="rm tempEBNdata.zip"
+os.system(myCmd)
